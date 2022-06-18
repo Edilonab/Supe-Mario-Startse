38c589f18fbaf876c0079ff1cd0afdf9f8b53f7f
Edilonab <90010997+Edilonab@users.noreply.github.com>
2022-06-17 23:18:35 -0300

    Final projeto base

--- js/script.js
+++ js/script.js
@@ -10,19 +10,26 @@ const jump = () => {
 };
 
 const loopGame = setInterval(()=> {
-    const pipePosition = pipe.offsetLeft
-}
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window
+        .getComputedStyle(mario)
+        .bottom.replace("px", "");
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
     pipe.style.animation = "none";
     pipe.style.left = `${pipePosition}px`;
 
     mario.style.animation = "none";
     mario.style.bottom = `${marioPosition}px`;
 
-    mario.src = "./Images/mario-game-over.png";
+    mario.src = "./Imagens/mario-game-over.png";
     mario.style.width = "75px";
     mario.style.marginLeft = "45px";
 
-    clearInterval(loopGame);
-)
+    clearInterval(loopGame)
+
+    }
+
+}, 10);
 
 document.addEventListener("keydown", jump);
